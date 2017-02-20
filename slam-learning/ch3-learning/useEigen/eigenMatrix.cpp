5b770af788dbc6491b719b289a0e19a498645232
xinliangzhong <xinliangzhong@foxmail.com>
2017-02-20 12:12:38 +0800

    use Eigen

--- slam-learning/ch3-learning/useEigen/eigenMatrix.cpp
+++ slam-learning/ch3-learning/useEigen/eigenMatrix.cpp
@@ -10,11 +10,44 @@ using namespace std;
 
 int main()
 {
-
-    cout << "useEigen" << endl;
+//    Eigen::MatrixXd m = Eigen::MatrixXd::Random(3,3);
+//    cout << m << endl;
     Eigen::Matrix<float,2,3> matrix_23;
+
+    Eigen::Vector3d v_3d;
+    Eigen::Matrix3d matrix_33 = Eigen::Matrix3d::Zero();
+    Eigen::MatrixXd matrix_x;
+
     matrix_23 << 1,2,3,4,5,6;
     cout << matrix_23 << endl;
+
+
+    v_3d << 3, 2, 1;
+    matrix_x = matrix_33.cast<double>() * v_3d;
+    cout << matrix_x << endl;
+
+    matrix_33 = Eigen::Matrix3d::Random();
+    cout << matrix_33 << endl << endl;
+
+    cout << matrix_33.transpose() << endl;
+    cout << matrix_33.sum() << endl;
+    cout << matrix_33.trace() << endl;
+    cout << 10 * matrix_33 << endl;
+    cout << matrix_33.inverse() << endl;
+    cout << matrix_33.determinant() << endl;
+
+    Eigen::Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN;
+    matrix_NN = Eigen::MatrixXd::Random(MATRIX_SIZE,MATRIX_SIZE);
+    Eigen::Matrix<double, MATRIX_SIZE, 1> v_Nd;
+    v_Nd = Eigen::MatrixXd::Random(MATRIX_SIZE, 1);
+
+    clock_t time_stt = clock();
+    Eigen::Matrix<double, MATRIX_SIZE, 1> x = matrix_NN.inverse() * v_Nd;
+    cout << "time use in normal inverse is " << 1000 * (clock() - time_stt) / (double)CLOCKS_PER_SEC << "ms" << endl;
+
+    time_stt = clock();
+    x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+    cout << "time use in Qr compsition is " << 1000 * (clock() - time_stt) / (double)CLOCKS_PER_SEC << "ms" << endl;
 
     return 0;
 }
