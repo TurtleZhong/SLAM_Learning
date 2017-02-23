eea5623bb53a94f29a1f114361eb6749341a1ede
xinliangzhong <xinliangzhong@foxmail.com>
2017-02-23 17:21:17 +0800

    try to read files

---
+++ c++_learning/readFiles/readfile.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+//    double data[5];
+//    for(int i = 0; i < 5; i++)
+//    {
+//        cin >> data[i];
+//    }
+
+//    for(int j =0; j < 5; j++)
+//    {
+//        cout << data[j] << endl;
+//    }
+
+
+    ifstream fin;
+    fin.open("../pose.txt");
+    if(!fin)
+    {
+        cout << "Error to open the file!" << endl;
+    }
+
+    double data[7];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            fin >> data[j];
+        }
+
+        cout << "***********" << endl;
+        for (int j = 0; j < 7; j++)
+        {
+            cout << data[j] << endl;
+        }
+
+    }
+
+
+    return 0;
+}
