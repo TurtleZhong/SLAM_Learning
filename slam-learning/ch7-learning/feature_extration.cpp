abb6e99be72825936c30e0939ecfbbe46f17344e
xinliangzhong <xinliangzhong@foxmail.com>
2017-03-11 12:05:56 +0800

    use orb to compute the feature

---
+++ slam-learning/ch7-learning/feature_extration.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv)
+{
+
+
+    /*
+      * step1: load the original image turn it into gray;
+      */
+
+    Mat srcImage = imread("../1.png", 1);
+    //imshow("original image", srcImage);
+    //     Mat grayImage;
+    //     cvtColor(srcImage, grayImage, CV_BGR2GRAY);
+
+    /*
+      * define the detect param: use ORB
+      */
+    OrbFeatureDetector featureDetector;
+    vector<KeyPoint> keyPoints;
+    Mat descriptors;
+
+    /*
+      * use detect() function to detect keypoints
+      */
+    featureDetector.detect(srcImage, keyPoints);
+
+    /*
+      * conpute the extractor and show the keypoints
+      */
+    OrbDescriptorExtractor featureEvaluator;
+    featureEvaluator.compute(srcImage, keyPoints, descriptors);
+
+    Mat pointsImage;
+    drawKeypoints(srcImage, keyPoints, pointsImage);
+    //imshow("keyPoints Image", pointsImage);
+    cout << "Totally we got "<< keyPoints.size() << " key points." << endl;
+
+    /*
+      * FLANN
+      */
+    flann::Index flannIndex(descriptors, flann::LshIndexParams(12, 20, 2), cvflann::FLANN_DIST_HAMMING);
+
+    /*
+      * Actually this is a sequence of image but this is just an example.
+      */
+    Mat testImage = imread("../2.png" , 1);
+    //     Mat testImage_gray;
+    //     cvtColor(testImage, testImage_gray, CV_BGR2GRAY);
+
+    vector<KeyPoint> testKeyPoints;
+    Mat testDescriptors;
+    featureDetector.detect(testImage, testKeyPoints);
+    featureEvaluator.compute(testImage, testKeyPoints,testDescriptors);
+
+    /*Match the feature*/
+    Mat matchIndex(testDescriptors.rows, 2, CV_32SC1);
+    Mat matchDistance(testDescriptors.rows, 2, CV_32SC1);
+    flannIndex.knnSearch(testDescriptors, matchIndex, matchDistance, 2, flann::SearchParams());
+
+    vector<DMatch> goodMatches;
+    for (int i = 0; i < matchDistance.rows; i++)
+    {
+        if(matchDistance.at<float>(i,0) < 0.6 * matchDistance.at<float>(i, 1))
+        {
+            DMatch dmatchs(i, matchIndex.at<int>(i,0), matchDistance.at<float>(i,1));
+            goodMatches.push_back(dmatchs);
+        }
+    }
+
+    Mat resultImage;
+    drawMatches(testImage, testKeyPoints, srcImage, keyPoints, goodMatches, resultImage);
+    imshow("result of Image", resultImage);
+
+    cout << "We got " << goodMatches.size() << " good Matchs" << endl;
+
+
+    waitKey(0);
+
+    return 0;
+}
